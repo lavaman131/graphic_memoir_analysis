ab801d8acd943bd3680f0a5cd11dceda2995c352
lavaman131 <lavaalex3@gmail.com>
2023-04-09 01:34:55 -0400

    fixed resource paths

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  site: 'https://alexlavaee.me/',
+  integrations: [
+    tailwind(),
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  site: "https://alexlavaee.me/",
   // base: '/graphic_memoir_analysis',
+  build: {
+    assetsPrefix: "./",
+  },
 });
